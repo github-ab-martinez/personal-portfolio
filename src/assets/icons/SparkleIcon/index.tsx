d130fe1ea559498d2ae8e0031e3bdaf50af3a35a
Samson Martinez <samsonmartinez@Samsons-MBP.lan>
2023-12-11 13:09:28 -0800

    Add availability banner

---
+++ src/assets/icons/SparkleIcon/index.tsx
@@ -0,0 +1,33 @@
+const SparkleIcon = () => {
+  return (
+    <svg
+      width="33"
+      height="33"
+      viewBox="0 0 33 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.211 0.241726C15.211 0.241726 16.5238 8.42413 20.1377 11.6781C23.7516 14.9321 32.0264 15.3824 32.0264 15.3824C32.0264 15.3824 23.844 16.6953 20.5901 20.3092C17.3361 23.9231 16.8857 32.1979 16.8857 32.1979C16.8857 32.1979 15.5729 24.0155 11.959 20.7615C8.34509 17.5075 0.0702854 17.0572 0.0702854 17.0572C0.0702854 17.0572 8.25269 15.7443 11.5067 12.1304C14.7606 8.51654 15.211 0.241726 15.211 0.241726Z"
+        fill="url(#paint0_linear_228_541)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_228_541"
+          x1="0.0676927"
+          y1="17.0077"
+          x2="32.0238"
+          y2="15.333"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.140625" stop-color="#B16CEA" />
+          <stop offset="0.447917" stop-color="#FF5E69" />
+          <stop offset="0.71875" stop-color="#FF8A56" />
+          <stop offset="1" stop-color="#FFA84B" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default SparkleIcon;
