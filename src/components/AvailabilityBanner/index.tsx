97b37b5a9e256a98d5cbc94606020c23f4875d68
Samson Martinez <samsonmartinez@Samsons-MBP.lan>
2023-12-16 11:21:25 -0800

    Availability banner styling and motion refinement

--- src/components/AvailabilityBanner/index.tsx
+++ src/components/AvailabilityBanner/index.tsx
@@ -1,66 +1,36 @@
-import SparkleIcon from '@/assets/icons/SparkleIcon';
-import Heading from '../common/Heading';
+"use client";
+
+import SparkleIcon from "@/assets/icons/SparkleIcon";
+import Heading from "../common/Heading";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+const availabilityContent = ["7+ Yrs Experience", "Available for Hire"];
 
 const AvailabilityBanner = () => {
+  const { scrollYProgress } = useScroll();
+  const translateX = useTransform(scrollYProgress, [0, 0.25], ["-200%", "0%"]);
+
   return (
-    <section className="bg-gradient-to-r from-purple via-red to-orange py-14 relative overflow-x-hidden">
-      <div className="bg-white-primary py-5 text-black-primary absolute w-100 -rotate-2 top-1 -left-1 flex gap-6 overflow-hidden">
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          7+ Yrs Experience <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          Available for Hire <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          7+ Yrs Experience <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          Available for Hire <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          7+ Yrs Experience <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          Available for Hire <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          7+ Yrs Experience <SparkleIcon />
-        </Heading>
-        <Heading
-          className="font-semibold flex items-center gap-6 whitespace-nowrap"
-          level="h4"
-          as="h2"
-        >
-          Available for Hire <SparkleIcon />
-        </Heading>
+    <section className="relative mb-[-30px] mt-[-30px] overflow-hidden py-12 before:absolute before:bottom-[30px] before:h-[calc(100%-60px)] before:w-full before:bg-gradient-to-r before:from-purple  before:via-red before:to-orange before:content-['']">
+      <div className="relative left-[-25%] top-0 flex w-[calc(150%)] -rotate-3 gap-6 bg-white-primary py-6 text-black-primary">
+        {availabilityContent
+          .concat(...Array(3).fill(availabilityContent))
+          .map((content, index) => (
+            <motion.span
+              style={{ translateX }}
+              className="flex gap-6"
+              key={index}
+            >
+              <Heading
+                className="flex items-center gap-6 whitespace-nowrap font-semibold"
+                level="h4"
+                as="h2"
+              >
+                {content}
+              </Heading>
+              <SparkleIcon />
+            </motion.span>
+          ))}
       </div>
     </section>
   );
--- src/components/common/AppWrapper/index.tsx
+++ src/components/common/AppWrapper/index.tsx
@@ -1,14 +1,14 @@
-import { FC, PropsWithChildren } from 'react';
-import { useDarkMode } from '@/components/common/StickyHeader/DarkModeToggle/DarkModeProvider';
-import { notoSans } from '@/app/fonts';
+import { FC, PropsWithChildren } from "react";
+import { useDarkMode } from "@/components/common/StickyHeader/DarkModeToggle/DarkModeProvider";
+import { notoSans } from "@/app/fonts";
 
 const AppWrapper: FC<PropsWithChildren> = ({ children }) => {
   const { darkMode } = useDarkMode();
 
   return (
-    <html lang="en" className={darkMode ? 'dark' : ''}>
+    <html lang="en" className={darkMode ? "dark" : ""}>
       <body
-        className={`${notoSans.className} bg-white-secondary text-black-secondary dark:bg-black-primary dark:text-white-primary max-w-full overflow-x-hidden`}
+        className={`${notoSans.className} max-w-full overflow-x-hidden bg-white-secondary text-black-secondary dark:bg-black-primary dark:text-white-primary`}
       >
         {darkMode === undefined ? null : <>{children}</>}
       </body>
