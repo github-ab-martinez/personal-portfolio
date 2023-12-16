ceab39041175750ed6330f396d3062b16603e489
Samson Martinez <samsonmartinez@Samsons-MBP.lan>
2023-12-16 11:34:33 -0800

    Refine Skills section styling

--- src/components/ToolsSection/ToolCard/index.tsx
+++ src/components/ToolsSection/ToolCard/index.tsx
@@ -1,6 +1,6 @@
-import { SVGLogo } from '@/assets/logos/types';
-import Heading from '@/components/common/Heading';
-import { FC } from 'react';
+import { SVGLogo } from "@/assets/logos/types";
+import Heading from "@/components/common/Heading";
+import { FC } from "react";
 
 interface ToolCardProps {
   darkMode: boolean;
@@ -10,8 +10,8 @@ interface ToolCardProps {
 
 const ToolCard: FC<ToolCardProps> = ({ name, logo: Logo, darkMode }) => {
   return (
-    <div className="bg-white-primary dark:bg-black-secondary p-8 rounded-md text-center flex flex-col items-center">
-      <span className="h-[100px] flex items-center">
+    <div className="flex flex-col items-center rounded-md bg-white-primary p-8 text-center dark:bg-black-secondary">
+      <span className="flex h-[60px] w-[60px] items-center">
         <Logo darkMode={darkMode ?? true} />
       </span>
       <Heading className="mt-4" level="h5" as="h3">
