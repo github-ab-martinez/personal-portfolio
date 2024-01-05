fd462524b05ce99b2f25fafde64c8c75d087f9bf
Samson Martinez <samsonmartinez@Samsons-MBP.lan>
2024-01-05 15:11:28 -0800

    Match footer padding

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -2,8 +2,8 @@ import AbLogo from "@/assets/logos/AbLogo";
 
 const Footer = () => {
   return (
-    <footer className=" bg-white-primary py-12 font-light dark:bg-black-secondary">
-      <div className="container flex flex-col items-center lg:flex-row lg:justify-between">
+    <footer className="bg-white-primary py-12 font-light dark:bg-black-secondary">
+      <div className="flex flex-col items-center px-8 2xl:container lg:flex-row lg:justify-between">
         <p className="mb-6 lg:mb-0">
           &copy; 2024 - Built from scratch by A.b. Martinez
         </p>
