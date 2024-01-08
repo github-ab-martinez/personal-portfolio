1c402e8d0889d709cd9eb295c65de0728a722630
Samson Martinez <samsonmartinez@Samsons-MBP.lan>
2024-01-08 14:43:55 -0800

    Fixed date formatting

--- src/components/ExperienceSection/index.tsx
+++ src/components/ExperienceSection/index.tsx
@@ -7,21 +7,22 @@ import { useNavSpy } from "../common/StickyHeader/HeaderNav/NavSpyProvider";
 import { useWorkHistory } from "./useWorkHistory";
 
 const months: Record<number, string> = {
-  1: "January",
-  2: "February",
-  3: "March",
-  4: "April",
-  5: "May",
-  6: "June",
-  7: "July",
-  8: "August",
-  9: "September",
-  10: "October",
-  11: "November",
-  12: "December",
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
 };
 
 const getFormattedDate = (date: Date) => {
+  console.log(date.getMonth());
   return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 };
 
